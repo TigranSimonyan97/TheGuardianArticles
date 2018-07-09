849e949452f50c4482f5bd4ca400cb0eb7e5c001
Tigran Simonyan <tigsim97@icloud.com>
2018-07-09 17:16:58 +0400

    Initial Commit

---
+++ TheGuardianArticles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheGuardianArticles
+//
+//  Created by Tigran Simonyan on 7/9/18.
+//  Copyright © 2018 TigransApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
